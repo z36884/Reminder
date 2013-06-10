ac0f7acab618c8be3a27564d6ee314bdb58300fd
Stanley Ding <stanley811213@gmail.com>
2013-06-10 22:19:54 +0800

    CreateEventForm can now be dragged. GUI needs improvements.

--- CreateEventForm.cs
+++ CreateEventForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Reminder
 {
@@ -10,8 +11,11 @@ namespace Reminder
         TextBox nameTextBox;
         DateTimePicker picker;
         ComboBox typeComboBox;
-        Button ok;
+        Button ok, cancel;
         private const int groupboxWidth = 250;
+        bool drag;
+        Point startPoint;
+        Point formStartPoint;
 
         public CreateEventForm()
         {
@@ -23,12 +27,15 @@ namespace Reminder
             nameTextBox = new TextBox();
             nameTextBox.Location = new Point(5, 20);
             nameTextBox.Name = "nameTextBox";
+            nameTextBox.KeyDown += keyDown;
 
             // name group box
             title.Location = new Point(10, 10);
-            title.Size = new Size(groupboxWidth, 50);
+            title.BackColor = Color.Gray;
+            title.Size = new Size(groupboxWidth, 70);
             title.Name = "title";
             title.Text = "Title";
+            title.Font = new Font(title.Font.FontFamily, 20F);
             title.Controls.Add(nameTextBox);
 
             // due date time picker
@@ -36,13 +43,16 @@ namespace Reminder
             picker.Location = new Point(5, 20);
             picker.Name = "picker";
             picker.Format = DateTimePickerFormat.Custom;
-            picker.CustomFormat = "yyyy.MMM.dd   hh:mm:ss";
+            picker.CustomFormat = "yyyy.MMM.dd   hh:mm";
+            picker.KeyDown += keyDown;
 
             // due group box
             due.Location = new Point(10, 70);
-            due.Size = new Size(groupboxWidth, 50);
+            due.BackColor = Color.Gray;
+            due.Size = new Size(groupboxWidth, 70);
             due.Name = "due";
             due.Text = "Due";
+            due.Font = new Font(due.Font.FontFamily, 20F);
             due.Controls.Add(picker);
 
             // type combo box
@@ -51,21 +61,32 @@ namespace Reminder
             typeComboBox.Name = "typeComboBox";
             typeComboBox.Items.Add("Single");
             typeComboBox.Items.Add("Multiple");
+            typeComboBox.KeyDown += keyDown;
 
             // type group box
             type.Location = new Point(10, 130);
-            type.Size = new Size(groupboxWidth, 50);
+            type.BackColor = Color.Gray;
+            type.Size = new Size(groupboxWidth, 70);
             type.Name = "type";
             type.Text = "Type";
+            type.Font = new Font(type.Font.FontFamily, 20F);
             type.Controls.Add(typeComboBox);
 
-            // buttons group box
+            // buttons
             Button ok = new Button();
+            ok.BackColor = Color.Gray;
             ok.Location = new Point(10, 200);
             ok.Name = "ok";
             ok.Text = "Add";
 
             // CreateEventForm
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MouseDown += formMouseDown;
+            this.MouseUp += formMouseUp;
+            this.MouseMove += formMouseMove;
+            this.BackColor = Color.Lime;
+            this.TransparencyKey = Color.Lime;
+            this.Paint += form_Paint;
             this.Controls.Add(title);
             this.Controls.Add(due);
             this.Controls.Add(type);
@@ -92,6 +113,64 @@ namespace Reminder
             {
                 return typeComboBox;
             }
+        }
+
+        public void DrawRoundRect(Graphics g, Pen p, float X, float Y, float width, float height, float radius)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            //Upper-right arc:
+            gp.AddArc(X + width - (radius * 2), Y, radius * 2, radius * 2, 270, 90);
+            //Lower-right arc:
+            gp.AddArc(X + width - (radius * 2), Y + height - (radius * 2), radius * 2, radius * 2, 0, 90);
+            //Lower-left arc:
+            gp.AddArc(X, Y + height - (radius * 2), radius * 2, radius * 2, 90, 90);
+            //Upper-left arc:
+            gp.AddArc(X, Y, radius * 2, radius * 2, 180, 90);
+            gp.CloseFigure();
+            g.FillPath(Brushes.Gray, gp);
+            gp.Dispose();
+        }
+
+        public void keyDown(object senderm, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    this.Dispose();
+                    break;
+                case Keys.Enter:
+                    Console.WriteLine("enter");
+                    break;
+            }
+        }
+
+        void formMouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = this.PointToScreen(e.Location);
+            formStartPoint = this.FindForm().Location;
+            this.drag = true;
+        }
+
+        void formMouseUp(object sender, MouseEventArgs e)
+        {
+            this.drag = false;
+        }
+
+        void formMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.drag)
+            {
+                int XDelta, YDelta;
+                Point endPoint = this.PointToScreen(e.Location);
+                XDelta = endPoint.X - startPoint.X;
+                YDelta = endPoint.Y - startPoint.Y;
+                this.FindForm().Location = new Point(formStartPoint.X + XDelta, formStartPoint.Y + YDelta);
+            }
+        }
+
+        public void form_Paint(object sender, PaintEventArgs e)
+        {
+            DrawRoundRect(e.Graphics, Pens.Black, e.ClipRectangle.Left, e.ClipRectangle.Top, e.ClipRectangle.Width, e.ClipRectangle.Height, 10);
         }
     }
 }
--- MainFormComponent.cs
+++ MainFormComponent.cs
@@ -9,17 +9,29 @@ namespace Reminder
         private const int WM_NCHITTEST = 0x84;
         private const int HTCLIENT = 0x1;
         private const int HTCAPTION = 0x2;
+        private const int MainFormWidth = 300;
+        private const int MainFormHeight = 500;
+
+        TitleBar titleBar;
+        PictureBox adder;
 
         private void InitializeComponent()
         {
             this.SuspendLayout();
 
-            // TitleBar
-            TitleBar titleBar = new TitleBar();
-            titleBar.Size = new Size(300, 40);
-            titleBar.Location = new Point(0, 0);
+            // titleBar
+            titleBar = new TitleBar();
+            titleBar.Size = new Size(300 - 2, 40);
+            titleBar.Location = new Point(0 + 1, 1);
             titleBar.BackColor = Color.DimGray;
 
+            // adder
+            adder = new PictureBox();
+            adder.Size = new Size(50, 50);
+            adder.Location = new Point(0, 50);
+            adder.BackColor = Color.Aqua;
+            adder.MouseClick += adder_MouseClick;
+            adder.Paint += adder_Paint;
           
             // MainForm
             this.FormBorderStyle = FormBorderStyle.None;
@@ -27,19 +39,46 @@ namespace Reminder
             this.Size = new Size(300, 500);
             this.BackColor = Color.DimGray;
             this.Controls.Add(titleBar);
+            this.Controls.Add(adder);
+            this.Paint += form_Paint;
 
             this.ResumeLayout(false);
         }
 
-        //
         // Enable dragging from anywhere
-        //
         protected override void WndProc(ref Message message)
         {
             base.WndProc(ref message);
 
             if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
                 message.Result = (IntPtr)HTCAPTION;
+        }
+
+        public void form_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point topLeft = new Point(e.ClipRectangle.Left, e.ClipRectangle.Top);
+            Point topRight = new Point(e.ClipRectangle.Right - 1, e.ClipRectangle.Top);
+            Point bottomLeft = new Point(e.ClipRectangle.Left, e.ClipRectangle.Bottom - 1);
+            Point bottomRight = new Point(e.ClipRectangle.Right - 1, e.ClipRectangle.Bottom - 1);
+            Pen pen = new Pen(Color.Blue);
+            g.DrawLine(pen, topLeft, topRight);
+            g.DrawLine(pen, topLeft, bottomLeft);
+            g.DrawLine(pen, topRight, bottomRight);
+            g.DrawLine(pen, bottomLeft, bottomRight);
+        }
+
+        public void adder_MouseClick(object sender, MouseEventArgs e)
+        {
+            CreateEventForm form = new CreateEventForm();
+            form.ShowDialog();
+        }
+
+        public void adder_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Red);
+            g.DrawString("Add", new Font("Segoe Script", 10F), Brushes.Black, new PointF(5, 5));
         }
     }
 }
