966da0cbde8f4171f913cc39e4e710553527b4b5
Jimmy Wang <Coffeinism@gmail.com>
2013-06-13 11:44:47 +0800

    改變 MultiEvent 大小

--- Event.cs
+++ Event.cs
@@ -7,15 +7,19 @@ namespace Reminder
 {
     class Event : Panel
     {
-        private Panel eventPanel;
-        private Panel controlPanel;
-        private List<EventClass> ec;
-        private int position;
+	private Panel eventPanel;
+	private Panel controlPanel;
+	private List<EventClass> ec;
+	private int position;
+	private Label name;
+	private Label subtitle;
+	private Label date;
+	private Label importance;
 
-        public Event(List<EventClass> ec, int position)
-        {
-            this.ec = ec;
-            this.position = position;
+	public Event(List<EventClass> ec, int position)
+	{
+	    this.ec = ec;
+	    this.position = position;
 
 	    if(ec[position].IsMultiEvent == false)
 		setSingleEvent();
@@ -24,166 +28,252 @@ namespace Reminder
 
 	    setControlPanel();
 
-            eventPanel.Click += new EventHandler(eventPanel_MouseClick);
-            controlPanel.Click += new EventHandler(controlPanel_MouseClick);
-        }
+	    eventPanel.Click += new EventHandler(eventPanel_MouseClick);
+	    controlPanel.Click += new EventHandler(controlPanel_MouseClick);
+	}
 
-        private void eventPanel_MouseClick(object sender, EventArgs e)
-        {
-            eventPanel.Visible = false;
-            controlPanel.Visible = true;
-        }
+	private void eventPanel_MouseClick(object sender, EventArgs e)
+	{
+	    eventPanel.Visible = false;
+	    controlPanel.Visible = true;
+	}
 
-        private void controlPanel_MouseClick(object sender, EventArgs e)
-        {
-            eventPanel.Visible = true;
-            controlPanel.Visible = false;
-        }
+	private void controlPanel_MouseClick(object sender, EventArgs e)
+	{
+	    eventPanel.Visible = true;
+	    controlPanel.Visible = false;
+	}
 
-        public void check_MouseClick(object sender, EventArgs e)
-        {
-            ec[position].IsFinished = true;
-            EventWriter.SerializeToXML(ec);
-            Control control = this.Parent;
-            control.Controls.Clear();
-            List<EventClass> ec2 = EventReader.DeserializeFromXML();
+	public void check_MouseClick(object sender, EventArgs e)
+	{
+	    ec[position].IsFinished = true;
+	    EventWriter.SerializeToXML(ec);
+	    Control control = this.Parent;
+	    control.Controls.Clear();
+	    List<EventClass> ec2 = EventReader.DeserializeFromXML();
 	    int b = 0;
-            for (int i = 0; i < ec2.Count; i++)
-            {
-		if(ec[i].IsFinished != true)
+	    int c = 0;
+	    for (int i = 0; i < ec2.Count; i++)
+	    {
+		if(ec2[i].IsFinished != true)
 		{
-		    Event list = new Event(ec2, i);
-		    list.Size = new Size(300, 60);
-		    list.Location = new Point(0, (b++) * 60 + 40);
+		    Event list = new Event(ec, i);
+		    if(ec2[i].IsMultiEvent == false)
+		    {
+			list.Size = new Size(300, 60);
+			list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+		    }
+		    else
+		    {
+			list.Size = new Size(300, 90);
+			list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+		    }
 		    control.Controls.Add(list);
 		}
-            }
-        }
+	    }
+	}
 
-        public void delete_MouseClick(object sender, EventArgs e)
-        {
-            ec.RemoveAt(position);
-            EventWriter.SerializeToXML(ec);
-            Control control = this.Parent;
-            control.Controls.Clear();
-            List<EventClass> ec2 = EventReader.DeserializeFromXML();
+	public void check_MouseClick_Multi(object sender, EventArgs e)
+	{
+	    ec[position].Eventlist.RemoveAt(0);
+	    if(ec[position].Eventlist.Count == 0)
+	    {
+		ec[position].IsFinished = true;
+		ec[position].IsMultiEvent = false;
+		EventWriter.SerializeToXML(ec);
+		Control control = this.Parent;
+		control.Controls.Clear();
+		List<EventClass> ec2 = EventReader.DeserializeFromXML();
+		int b = 0;
+		int c = 0;
+		for (int i = 0; i < ec.Count; i++)
+		{
+		    if(ec2[i].IsFinished != true)
+		    {
+			Event list = new Event(ec, i);
+			if(ec2[i].IsMultiEvent == false)
+			{
+			    list.Size = new Size(300, 60);
+			    list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+			}
+			else
+			{
+			    list.Size = new Size(300, 90);
+			    list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+			}
+			control.Controls.Add(list);
+		    }
+		}
+	    }
+	    else
+	    {
+		subtitle.Text = ec[position].Eventlist[0];
+		eventPanel.Visible = true;
+		controlPanel.Visible = false;
+	    }
+
+	}
+
+	public void delete_MouseClick(object sender, EventArgs e)
+	{
+	    ec.RemoveAt(position);
+	    EventWriter.SerializeToXML(ec);
+	    Control control = this.Parent;
+	    control.Controls.Clear();
+	    List<EventClass> ec2 = EventReader.DeserializeFromXML();
 	    int b = 0;
-            for (int i = 0; i < ec2.Count; i++)
-            {
-		if(ec[i].IsFinished != true)
+	    int c = 0;
+	    for (int i = 0; i < ec2.Count; i++)
+	    {
+		if(ec2[i].IsFinished != true)
 		{
 		    Event list = new Event(ec2, i);
-		    list.Size = new Size(300, 60);
-		    list.Location = new Point(0, (b++) * 60 + 40);
+		    if(ec2[i].IsMultiEvent == false)
+		    {
+			list.Size = new Size(300, 60);
+			list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+		    }
+		    else
+		    {
+			list.Size = new Size(300, 90);
+			list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+		    }
 		    control.Controls.Add(list);
 		}
-            }
-        }
+	    }
+	}
 
-        public void edit_MouseClick(object sender, EventArgs e)
-        {
-            CreateEventForm form = new CreateEventForm(ec[position]);
-            form.ShowDialog();
-            eventPanel.Visible = true;
-            controlPanel.Visible = false;
-        }
+	public void edit_MouseClick(object sender, EventArgs e)
+	{
+	    CreateEventForm form = new CreateEventForm(ec[position]);
+	    form.ShowDialog();
+	    eventPanel.Visible = true;
+	    controlPanel.Visible = false;
+	}
 
 	public void setSingleEvent()
 	{
 	    setText();
+	    eventPanel.Size = new Size(300, 60);
+	    importance.Location = new Point(0, 30);
+	    date.Location = new Point(60, 30);
 
-            Label name = new Label();
-            name.Text = ec[position].Name;
-            name.Location = new Point(0, 0);
-            name.Size = new Size(300, 40);
-            name.Font = new Font("Segoe Script", 20F);
-            name.Click += new EventHandler(eventPanel_MouseClick);
-            eventPanel.Controls.Add(name);
+	    name = new Label();
+	    name.Text = ec[position].Name;
+	    name.Location = new Point(0, 0);
+	    name.Size = new Size(300, 40);
+	    name.Font = new Font("Segoe Script", 20F);
+	    name.Click += new EventHandler(eventPanel_MouseClick);
+	    eventPanel.Controls.Add(name);
+
 	}
 
 	public void setMultiEvent()
 	{
 	    setText();
 
-            Label name = new Label();
-            name.Text = ec[position].Name;
-            name.Location = new Point(200, 4);
-            name.Size = new Size(100, 40);
-            name.Font = new Font("Segoe Script", 16F);
-            name.Click += new EventHandler(eventPanel_MouseClick);
-	    
-            Label subtitle = new Label();
-            subtitle.Text = ec[position].Eventlist[0];
-            subtitle.Location = new Point(0, 0);
-            subtitle.Size = new Size(200, 40);
-            subtitle.Font = new Font("Segoe Script", 20F);
-            subtitle.Click += new EventHandler(eventPanel_MouseClick);
+	    name = new Label();
+	    name.Text = ec[position].Name;
+	    name.Size = new Size(100, 40);
+	    name.Font = new Font("Segoe Script", 16F);
+	    name.Click += new EventHandler(eventPanel_MouseClick);
 
-            eventPanel.Controls.Add(name);
-            eventPanel.Controls.Add(subtitle);
+	    subtitle = new Label();
+	    subtitle.Text = ec[position].Eventlist[0];
+	    subtitle.Size = new Size(100, 40);
+	    subtitle.Font = new Font("Segoe Script", 20F);
+	    subtitle.Click += new EventHandler(eventPanel_MouseClick);
+
+	    eventPanel.Size = new Size(300, 90);
+	    subtitle.Location = new Point(0, 0);
+	    name.Location = new Point(0, 30);
+	    importance.Location = new Point(0, 55);
+	    date.Location = new Point(60, 55);
+
+	    eventPanel.Controls.Add(name);
+	    eventPanel.Controls.Add(subtitle);
 	}
 
 	public void setText()
 	{
-            eventPanel = new Panel();
+	    eventPanel = new Panel();
 	    if(ec[position].Due<DateTime.Now)
 		eventPanel.BackColor = Color.Blue;
 	    else
 		eventPanel.BackColor = Color.Green;
-            eventPanel.Visible = true;
-            eventPanel.Size = new Size(300, 60);
+	    eventPanel.Visible = true;
 
-            Label importance = new Label();
-            importance.Text = Convert.ToString(ec[position].Importance);
-            importance.Location = new Point(0, 30);
-            importance.Size = new Size(50, 40);
-            importance.Font = new Font("Segoe Script", 16F);
-            importance.Click += new EventHandler(eventPanel_MouseClick);
+	    importance = new Label();
+	    importance.Text = Convert.ToString(ec[position].Importance);
+	    importance.Size = new Size(50, 40);
+	    importance.Font = new Font("Segoe Script", 16F);
+	    importance.Click += new EventHandler(eventPanel_MouseClick);
 
-            Label date = new Label();
-            date.Text = ec[position].Due.ToString("yyyy/MMM/dd H:mm");
-            date.Location = new Point(60, 30);
-            date.Size = new Size(300, 40);
-            date.Font = new Font("Segoe Script", 16F);
-            date.Click += new EventHandler(eventPanel_MouseClick);
+	    date = new Label();
+	    date.Text = ec[position].Due.ToString("yyyy/MMM/dd H:mm");
+	    date.Size = new Size(300, 40);
+	    date.Font = new Font("Segoe Script", 16F);
+	    date.Click += new EventHandler(eventPanel_MouseClick);
 
-            eventPanel.Controls.Add(importance);
-            eventPanel.Controls.Add(date);
-            this.Controls.Add(eventPanel);
+	    eventPanel.Controls.Add(importance);
+	    eventPanel.Controls.Add(date);
+	    this.Controls.Add(eventPanel);
 	}
 
 	public void setControlPanel()
 	{
-            controlPanel = new Panel();
-            controlPanel.BackColor = Color.Red;
-            controlPanel.Size = new Size(300, 60);
-            controlPanel.Visible = false;
+	    controlPanel = new Panel();
+	    controlPanel.BackColor = Color.Red;
+	    if(ec[position].IsMultiEvent == false)
+		controlPanel.Size = new Size(300, 60);
+	    else
+		controlPanel.Size = new Size(300, 90);
+	    controlPanel.Visible = false;
 
-            PictureBox check = new PictureBox();
-            check.Image = Image.FromFile("check.png");
-            check.Location = new Point(25, 5);
-            check.Size = new Size(50, 50);
-            check.SizeMode = PictureBoxSizeMode.StretchImage;
-            check.Click += new EventHandler(check_MouseClick);
+	    PictureBox check = new PictureBox();
+	    PictureBox delete = new PictureBox();
+	    PictureBox edit = new PictureBox();
+	    check.Size = delete.Size = edit.Size = new Size(50, 50);
 
-            PictureBox delete = new PictureBox();
-            delete.Image = Image.FromFile("delete.png");
-            delete.Location = new Point(115, 5);
-            delete.Size = new Size(50, 50);
-            delete.SizeMode = PictureBoxSizeMode.StretchImage;
-            delete.Click += new EventHandler(delete_MouseClick);
+	    check.Image = Image.FromFile("check.png");
+	    if(ec[position].IsMultiEvent == false)
+		check.Click += new EventHandler(check_MouseClick);
+	    else
+		check.Click += new EventHandler(check_MouseClick_Multi);
 
-            PictureBox edit = new PictureBox();
-            edit.Image = Image.FromFile("edit.png");
-            edit.Location = new Point(205, 5);
-            edit.Size = new Size(50, 50);
-            edit.SizeMode = PictureBoxSizeMode.StretchImage;
-            edit.Click += new EventHandler(edit_MouseClick);
+	    delete.Image = Image.FromFile("delete.png");
+	    delete.Click += new EventHandler(delete_MouseClick);
 
-            controlPanel.Controls.Add(check);
-            controlPanel.Controls.Add(delete);
-            controlPanel.Controls.Add(edit);
-            this.Controls.Add(controlPanel);
+	    edit.Image = Image.FromFile("edit.png");
+	    edit.Click += new EventHandler(edit_MouseClick);
+
+	    if(ec[position].IsFinished == false)
+	    {
+		if(ec[position].IsMultiEvent == false)
+		{
+		    check.Location = new Point(25, 5);
+		    delete.Location = new Point(115, 5);
+		    edit.Location = new Point(205, 5);
+		}
+		else
+		{
+		    check.Location = new Point(25, 20);
+		    delete.Location = new Point(115, 20);
+		    edit.Location = new Point(205, 20);
+		}
+	    }
+	    else
+	    {
+		check.Visible = false;
+		delete.Location = new Point(75, 5);
+		edit.Location = new Point(165, 5);
+	    }
+
+	    check.SizeMode = delete.SizeMode = edit.SizeMode = PictureBoxSizeMode.StretchImage;
+	    controlPanel.Controls.Add(check);
+	    controlPanel.Controls.Add(delete);
+	    controlPanel.Controls.Add(edit);
+	    this.Controls.Add(controlPanel);
 	}
     }
 }
--- MainFormComponent.cs
+++ MainFormComponent.cs
@@ -8,144 +8,180 @@ namespace Reminder
 
     public partial class MainForm
     {
-        private const int MainFormWidth = 300;
-        private const int MainFormHeight = 500;
+	private const int MainFormWidth = 300;
+	private const int MainFormHeight = 500;
 
-        TitleBar titleBar;
-        PictureBox adder;
-        PictureBox sort;
-        PictureBox finish;
+	TitleBar titleBar;
+	PictureBox adder;
+	PictureBox sort;
+	PictureBox finish;
 
 	private Panel listPanel;
 	private int sortMethod = 1;
 
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
+	private void InitializeComponent()
+	{
+	    this.SuspendLayout();
 
-            // titleBar
-            titleBar = new TitleBar();
-            titleBar.Size = new Size(300 - 2, 40);
-            titleBar.Location = new Point(0 + 1, 1);
-            titleBar.BackColor = Color.DimGray;
+	    // titleBar
+	    titleBar = new TitleBar();
+	    titleBar.Size = new Size(300 - 2, 40);
+	    titleBar.Location = new Point(0 + 1, 1);
+	    titleBar.BackColor = Color.DimGray;
 
-            // adder
-            adder = new AddButton(30, 30);
-            adder.Location = new Point(0, 470);
-            adder.MouseClick += adder_MouseClick;
+	    // adder
+	    adder = new AddButton(30, 30);
+	    adder.Location = new Point(0, 470);
+	    adder.MouseClick += adder_MouseClick;
 
-            // sort 
-            sort = new AddButton(30, 30);
-            sort.Location = new Point(50, 470);
-            sort.MouseClick += sort_MouseClick;
-          
-            // finish 
-            finish = new AddButton(30, 30);
-            finish.Location = new Point(100, 470);
-            finish.MouseClick += finish_MouseClick;
+	    // sort 
+	    sort = new AddButton(30, 30);
+	    sort.Location = new Point(50, 470);
+	    sort.MouseClick += sort_MouseClick;
 
-            // MainForm
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.Name = "MainForm";
-            this.Size = new Size(300, 500);
-            this.BackColor = Color.FromArgb(45, 45, 48);
-            this.Controls.Add(titleBar);
-            this.Controls.Add(adder);
-            this.Controls.Add(sort);
-            this.Controls.Add(finish);
+	    // finish 
+	    finish = new AddButton(30, 30);
+	    finish.Location = new Point(100, 470);
+	    finish.MouseClick += finish_MouseClick;
+
+	    // MainForm
+	    this.FormBorderStyle = FormBorderStyle.None;
+	    this.Name = "MainForm";
+	    this.Size = new Size(300, 500);
+	    this.BackColor = Color.FromArgb(45, 45, 48);
+	    this.Controls.Add(titleBar);
+	    this.Controls.Add(adder);
+	    this.Controls.Add(sort);
+	    this.Controls.Add(finish);
 
 	    //Event
 	    listPanel = new Panel();
-            listPanel.Size = new Size(300, 500);
-            try
-            {
-                List<EventClass> ec = EventReader.DeserializeFromXML();
+	    listPanel.Size = new Size(300, 500);
+	    try
+	    {
+		List<EventClass> ec = EventReader.DeserializeFromXML();
 		int b = 0;
+		int c = 0;
 		for (int i = 0; i < ec.Count; i++)
 		{
 		    if(ec[i].IsFinished != true)
 		    {
 			Event list = new Event(ec, i);
-			list.Size = new Size(300, 60);
-			list.Location = new Point(0, (b++) * 60 + 40);
+			if(ec[i].IsMultiEvent == false)
+			{
+			    list.Size = new Size(300, 60);
+			    list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+			}
+			else
+			{
+			    list.Size = new Size(300, 90);
+			    list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+			}
 			listPanel.Controls.Add(list);
 		    }
 		}
 	    }
-            catch { }
+	    catch { }
 	    listPanel.AutoScroll = true;
-            this.Controls.Add(listPanel);
+	    this.Controls.Add(listPanel);
 
-            this.ResumeLayout(false);
-        }
-
-        public void adder_MouseClick(object sender, MouseEventArgs e)
-        {
-            CreateEventForm form = new CreateEventForm();
-            form.ShowDialog();
-            Sort();
-        }
-
-        public void sort_MouseClick(object sender, MouseEventArgs e)
-        {
-            Sort();
+	    this.ResumeLayout(false);
 	}
 
-        public void finish_MouseClick(object sender, MouseEventArgs e)
-        {
+	public void adder_MouseClick(object sender, MouseEventArgs e)
+	{
+	    CreateEventForm form = new CreateEventForm();
+	    form.ShowDialog();
+	    Sort();
+	}
+
+	public void sort_MouseClick(object sender, MouseEventArgs e)
+	{
+	    Sort();
+	}
+
+	public void finish_MouseClick(object sender, MouseEventArgs e)
+	{
 	    listPanel.Controls.Clear();
 	    List<EventClass> ec2 = EventReader.DeserializeFromXML();
 	    int b = 0;
-	    for(int i=0;i<ec2.Count;i++)
+	    int c = 0;
+	    for (int i = 0; i < ec2.Count; i++)
 	    {
 		if(ec2[i].IsFinished == true)
 		{
 		    Event list = new Event(ec2, i);
-		    list.Size = new Size(300, 60);
-		    list.Location = new Point(0, (b++)*60+40);
+		    if(ec2[i].IsMultiEvent == false)
+		    {
+			list.Size = new Size(300, 60);
+			list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+		    }
+		    else
+		    {
+			list.Size = new Size(300, 90);
+			list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+		    }
 		    listPanel.Controls.Add(list);
 		}
 	    }
-        }
+	}
 
-        public void Sort()
-        {
-            if (sortMethod == 1)
-            {
-                sortMethod = 0;
-                listPanel.Controls.Clear();
-                List<EventClass> ec2 = EventReader.DeserializeFromXML();
-                ec2.Sort((x, y) => { return x.Due.CompareTo(y.Due); });
+	public void Sort()
+	{
+	    if (sortMethod == 1)
+	    {
+		sortMethod = 0;
+		listPanel.Controls.Clear();
+		List<EventClass> ec2 = EventReader.DeserializeFromXML();
+		ec2.Sort((x, y) => { return x.Due.CompareTo(y.Due); });
 		int b = 0;
-                for (int i = 0; i < ec2.Count; i++)
-                {
+		int c = 0;
+		for (int i = 0; i < ec2.Count; i++)
+		{
 		    if(ec2[i].IsFinished != true)
 		    {
 			Event list = new Event(ec2, i);
-			list.Size = new Size(300, 60);
-			list.Location = new Point(0, (b++) * 60 + 40);
+			if(ec2[i].IsMultiEvent == false)
+			{
+			    list.Size = new Size(300, 60);
+			    list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+			}
+			else
+			{
+			    list.Size = new Size(300, 90);
+			    list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+			}
 			listPanel.Controls.Add(list);
 		    }
-                }
-            }
-            else if (sortMethod == 0)
-            {
-                sortMethod = 1;
-                listPanel.Controls.Clear();
-                List<EventClass> ec2 = EventReader.DeserializeFromXML();
-                ec2.Sort((x, y) => { return -x.Importance.CompareTo(y.Importance); });
+		}
+	    }
+	    else if (sortMethod == 0)
+	    {
+		sortMethod = 1;
+		listPanel.Controls.Clear();
+		List<EventClass> ec2 = EventReader.DeserializeFromXML();
+		ec2.Sort((x, y) => { return -x.Importance.CompareTo(y.Importance); });
 		int b = 0;
-                for (int i = 0; i < ec2.Count; i++)
-                {
+		int c = 0;
+		for (int i = 0; i < ec2.Count; i++)
+		{
 		    if(ec2[i].IsFinished != true)
 		    {
 			Event list = new Event(ec2, i);
-			list.Size = new Size(300, 60);
-			list.Location = new Point(0, (b++) * 60 + 40);
+			if(ec2[i].IsMultiEvent == false)
+			{
+			    list.Size = new Size(300, 60);
+			    list.Location = new Point(0, (b++) * 60 + c * 90 + 40);
+			}
+			else
+			{
+			    list.Size = new Size(300, 90);
+			    list.Location = new Point(0, b * 60 + (c++) * 90 + 40 );
+			}
 			listPanel.Controls.Add(list);
 		    }
-                }
-            }
-        }
+		}
+	    }
+	}
     }
 }
