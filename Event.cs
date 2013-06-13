c31b825e8dceac682a507223d1be2e07400b52b1
Jimmy Wang <Coffeinism@gmail.com>
2013-06-13 10:21:41 +0800

    依單一事件、多重事件分隔
    未載入資料

--- Event.cs
+++ Event.cs
@@ -17,70 +17,12 @@ namespace Reminder
             this.ec = ec;
             this.position = position;
 
-            eventPanel = new Panel();
-	    if(ec[position].Due<DateTime.Now)
-		eventPanel.BackColor = Color.Blue;
+	    if(ec[position].IsMultiEvent == false)
+		setSingleEvent();
 	    else
-		eventPanel.BackColor = Color.Green;
-            eventPanel.Visible = true;
-            eventPanel.Size = new Size(300, 60);
+		setMultiEvent();
 
-            Label name = new Label();
-            name.Text = ec[position].Name;
-            name.Location = new Point(0, 0);
-            name.Size = new Size(300, 40);
-            name.Font = new Font("Segoe Script", 20F);
-            name.Click += new EventHandler(eventPanel_MouseClick);
-
-            Label importance = new Label();
-            importance.Text = Convert.ToString(ec[position].Importance);
-            importance.Location = new Point(0, 30);
-            importance.Size = new Size(50, 40);
-            importance.Font = new Font("Segoe Script", 16F);
-            importance.Click += new EventHandler(eventPanel_MouseClick);
-
-            Label date = new Label();
-            date.Text = ec[position].Due.ToString("yyyy/MMM/dd H:mm");
-            date.Location = new Point(60, 30);
-            date.Size = new Size(300, 40);
-            date.Font = new Font("Segoe Script", 16F);
-            date.Click += new EventHandler(eventPanel_MouseClick);
-
-            eventPanel.Controls.Add(name);
-            eventPanel.Controls.Add(importance);
-            eventPanel.Controls.Add(date);
-            this.Controls.Add(eventPanel);
-
-            controlPanel = new Panel();
-            controlPanel.BackColor = Color.Red;
-            controlPanel.Size = new Size(300, 60);
-            controlPanel.Visible = false;
-
-            PictureBox check = new PictureBox();
-            check.Image = Image.FromFile("check.png");
-            check.Location = new Point(10, 5);
-            check.Size = new Size(50, 50);
-            check.SizeMode = PictureBoxSizeMode.StretchImage;
-            check.Click += new EventHandler(check_MouseClick);
-
-            PictureBox delete = new PictureBox();
-            delete.Image = Image.FromFile("delete.png");
-            delete.Location = new Point(70, 5);
-            delete.Size = new Size(50, 50);
-            delete.SizeMode = PictureBoxSizeMode.StretchImage;
-            delete.Click += new EventHandler(delete_MouseClick);
-
-            PictureBox edit = new PictureBox();
-            edit.Image = Image.FromFile("edit.png");
-            edit.Location = new Point(130, 5);
-            edit.Size = new Size(50, 50);
-            edit.SizeMode = PictureBoxSizeMode.StretchImage;
-            edit.Click += new EventHandler(edit_MouseClick);
-
-            controlPanel.Controls.Add(check);
-            controlPanel.Controls.Add(delete);
-            controlPanel.Controls.Add(edit);
-            this.Controls.Add(controlPanel);
+	    setControlPanel();
 
             eventPanel.Click += new EventHandler(eventPanel_MouseClick);
             controlPanel.Click += new EventHandler(controlPanel_MouseClick);
@@ -145,5 +87,104 @@ namespace Reminder
             eventPanel.Visible = true;
             controlPanel.Visible = false;
         }
+
+	public void setSingleEvent()
+	{
+	    setText();
+
+            Label name = new Label();
+            name.Text = ec[position].Name;
+            name.Location = new Point(0, 0);
+            name.Size = new Size(300, 40);
+            name.Font = new Font("Segoe Script", 20F);
+            name.Click += new EventHandler(eventPanel_MouseClick);
+            eventPanel.Controls.Add(name);
+	}
+
+	public void setMultiEvent()
+	{
+	    setText();
+
+            Label name = new Label();
+            name.Text = ec[position].Name;
+            name.Location = new Point(0, 0);
+            name.Size = new Size(100, 40);
+            name.Font = new Font("Segoe Script", 14F);
+            name.Click += new EventHandler(eventPanel_MouseClick);
+	    
+            Label subtitle = new Label();
+	    System.Console.WriteLine("AAA");
+            //subtitle.Text = ec[position].Eventlist[0];
+            subtitle.Location = new Point(100, 0);
+            subtitle.Size = new Size(200, 40);
+            subtitle.Font = new Font("Segoe Script", 20F);
+            subtitle.Click += new EventHandler(eventPanel_MouseClick);
+
+            eventPanel.Controls.Add(name);
+            eventPanel.Controls.Add(subtitle);
+	}
+
+	public void setText()
+	{
+            eventPanel = new Panel();
+	    if(ec[position].Due<DateTime.Now)
+		eventPanel.BackColor = Color.Blue;
+	    else
+		eventPanel.BackColor = Color.Green;
+            eventPanel.Visible = true;
+            eventPanel.Size = new Size(300, 60);
+
+            Label importance = new Label();
+            importance.Text = Convert.ToString(ec[position].Importance);
+            importance.Location = new Point(0, 30);
+            importance.Size = new Size(50, 40);
+            importance.Font = new Font("Segoe Script", 16F);
+            importance.Click += new EventHandler(eventPanel_MouseClick);
+
+            Label date = new Label();
+            date.Text = ec[position].Due.ToString("yyyy/MMM/dd H:mm");
+            date.Location = new Point(60, 30);
+            date.Size = new Size(300, 40);
+            date.Font = new Font("Segoe Script", 16F);
+            date.Click += new EventHandler(eventPanel_MouseClick);
+
+            eventPanel.Controls.Add(importance);
+            eventPanel.Controls.Add(date);
+            this.Controls.Add(eventPanel);
+	}
+
+	public void setControlPanel()
+	{
+            controlPanel = new Panel();
+            controlPanel.BackColor = Color.Red;
+            controlPanel.Size = new Size(300, 60);
+            controlPanel.Visible = false;
+
+            PictureBox check = new PictureBox();
+            check.Image = Image.FromFile("check.png");
+            check.Location = new Point(25, 5);
+            check.Size = new Size(50, 50);
+            check.SizeMode = PictureBoxSizeMode.StretchImage;
+            check.Click += new EventHandler(check_MouseClick);
+
+            PictureBox delete = new PictureBox();
+            delete.Image = Image.FromFile("delete.png");
+            delete.Location = new Point(115, 5);
+            delete.Size = new Size(50, 50);
+            delete.SizeMode = PictureBoxSizeMode.StretchImage;
+            delete.Click += new EventHandler(delete_MouseClick);
+
+            PictureBox edit = new PictureBox();
+            edit.Image = Image.FromFile("edit.png");
+            edit.Location = new Point(205, 5);
+            edit.Size = new Size(50, 50);
+            edit.SizeMode = PictureBoxSizeMode.StretchImage;
+            edit.Click += new EventHandler(edit_MouseClick);
+
+            controlPanel.Controls.Add(check);
+            controlPanel.Controls.Add(delete);
+            controlPanel.Controls.Add(edit);
+            this.Controls.Add(controlPanel);
+	}
     }
 }
